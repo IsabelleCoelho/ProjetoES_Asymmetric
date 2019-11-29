6faa87ac056927f13594bff24158c7e2533e1725
Wixmix <vitor.borges@computacao.ufla.br>
2019-11-29 20:47:14 -0300

    close: #41

---
+++ back-end/asymmetric.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA IF NOT EXISTS `asymmetric` DEFAULT CHARACTER SET utf8 ;
+USE `asymmetric`;
+
+CREATE TABLE IF NOT EXISTS `asymmetric`.`obra` (
+  `idObra` INT NOT NULL AUTO_INCREMENT,
+  `nomeObra` VARCHAR(20) NOT NULL,
+  `valorEstimado` BIGINT NOT NULL,
+  `material` VARCHAR(10) NOT NULL,
+  `local` VARCHAR(15) NOT NULL,
+  `nomeAutor` VARCHAR(15) NOT NULL,
+  `foto` VARCHAR(30) NOT NULL,
+  `largura` FLOAT NOT NULL,
+  `altura` FLOAT NOT NULL,
+  `tipoFundo` INT NOT NULL,
+  PRIMARY KEY (`idObra`),
+  INDEX (`nomeObra`))
+ENGINE = InnoDB;
+
+use `asymmetric`;
+SELECT * FROM `obra`;
+
+DROP TABLE `asymmetric`.`obra`;
